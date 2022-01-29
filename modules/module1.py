f861777d33df7d9d93f3edce2aec9492daa734e7
wenhaofang <wenhaofang1998@qq.com>
2022-01-29 10:50:12 +0800

    add: module GreedySearch and BeamSearch

--- modules/module1.py
+++ modules/module1.py
@@ -174,6 +174,22 @@ class Decoder(nn.Module): # With Attention
 
         return predictions, dec_hiddens
 
+class BeamSearchNode():
+    def __init__(self, idx, pro, hid, prev_node):
+        self.idx = idx
+        self.pro = pro
+        self.hid = hid
+        self.prev_node = prev_node
+
+        if  prev_node is None:
+            self.sent_pro = pro
+            self.sent_len = 1
+        else:
+            self.sent_pro = prev_node.sent_pro + pro
+            self.sent_len = prev_node.sent_len + 1
+        
+        self.sent_matrix = self.sent_pro / self.sent_len
+
 class Seq2Seq(nn.Module):
     def __init__( self, 
         src_vocab_size, src_padded_idx, enc_emb_dim, enc_hid_dim, enc_dropout,
@@ -194,6 +210,13 @@ class Seq2Seq(nn.Module):
         )
 
     def forward(self, src, src_len, trg, teacher_forcing_ratio = 0.5):
+        '''
+        Params:
+            src    : Torch LongTensor (batch_size, src_seq_len)
+            src_len: Torch LongTensor (batch_size)
+            trg    : Torch LongTensor (batch_size, trg_seq_len)
+            teacher_forcing_ratio: Float between 0 and 1, probability of whether to use guide
+        '''
         encoder_outputs, encoder_hiddens = self.encoder(src, src_len)
 
         masked = src != self.src_padded_idx
@@ -205,10 +228,87 @@ class Seq2Seq(nn.Module):
 
         for t in range(1, trg_seq_len):
             output , hidden = self.decoder(inputs , hidden , encoder_outputs , masked)
-            inputs = trg[:, t] if random.random() < teacher_forcing_ratio else output.argmax(1)
+            inputs = trg[:, t] if random.random() < teacher_forcing_ratio else output.argmax(-1)
             outputs[:, t, :] = output
 
         return outputs
+
+    def predict(self, src, src_len, max_len, trg_bos_idx, trg_eos_idx, decoding_strategy, beam_width = 1):
+        '''
+        Params:
+            src    : Torch LongTensor (batch_size = 1, src_seq_len)
+            src_len: Torch LongTensor (batch_size = 1)
+            max_len: Int , maximum length of generated sentence
+            trg_bos_idx: Int , index of bos tag in trg vocab
+            trg_eos_idx: Int , index of eos tag in trg vocab
+            decoding_strategy: String, chosen between GreedySearch and BeamSearch
+        Return:
+            outputs: List<List<Int>>
+        '''
+        assert src.shape[0] == 1 ,  'predict method only support `batch_size == 1`'
+        assert decoding_strategy == 'greedysearch' or decoding_strategy == 'beamsearch'
+
+        encoder_outputs, encoder_hiddens = self.encoder(src, src_len)
+
+        masked = src != self.src_padded_idx
+        inputs = torch.ones(src.shape[0], dtype = src.dtype, device = src.device) * trg_bos_idx
+        hidden = encoder_hiddens
+
+        if  decoding_strategy == 'greedysearch':
+            outputs = []
+
+            for _ in range(max_len):
+                output , hidden = self.decoder(inputs, hidden, encoder_outputs, masked)
+                inputs = output.argmax(-1)
+                if  trg_eos_idx != inputs.item():
+                    outputs.append(inputs.item())
+                else:
+                    break
+
+            return [outputs]
+
+        if  decoding_strategy == 'beamsearch':
+            bst_nodes = []
+            end_nodes = []
+            decoding_step = 0
+
+            output , hidden = self.decoder(inputs, hidden, encoder_outputs, masked)
+            output = output.squeeze(0).log_softmax(0)
+            topk_p , topk_i = torch.topk(output, beam_width)
+            for i, p in zip(topk_i, topk_p):
+                bst_nodes.append(BeamSearchNode(i, p, hidden, None))
+
+            while len(end_nodes) < beam_width:
+                decoding_step += 1
+                tmp_nodes = []
+                for node in bst_nodes:
+                    output , hidden = self.decoder(node.idx.reshape(-1), node.hid, encoder_outputs, masked)
+                    output = output.squeeze(0).log_softmax(0)
+                    topk_p , topk_i = torch.topk(output, beam_width - len(end_nodes))
+                    for i, p in zip(topk_i, topk_p):
+                        tmp_nodes.append(BeamSearchNode(i, p, hidden, node))
+
+                tmp_nodes = sorted(tmp_nodes, key = lambda x: x.sent_matrix, reverse = True)[:beam_width - len(end_nodes)]
+
+                if  decoding_step == max_len:
+                    end_nodes.extend(tmp_nodes)
+                else:
+                    bst_nodes.clear()
+                    for node in tmp_nodes:
+                        if  node.idx == trg_eos_idx:
+                            end_nodes.append(node)
+                        else:
+                            bst_nodes.append(node)
+
+            outputs = []
+            for node in end_nodes:
+                tmp_outputs = []
+                while node is not None:
+                    tmp_outputs.append(node.idx.item())
+                    node = node.prev_node
+                outputs.append(tmp_outputs[::-1])
+
+            return outputs
 
 def get_module(
     option, src_vocab_size, trg_vocab_size, src_padded_idx, trg_padded_idx
@@ -247,3 +347,9 @@ if __name__ == '__main__':
 
     outputs = module(src, src_len, trg)
     print(outputs.shape) # (batch_size, trg_seq_len, trg_vocab_size)
+
+    # predict
+    g_result = module.predict(src[0].unsqueeze(0), src_len[0].unsqueeze(0), trg_seq_len, 0, 1, 'greedysearch' )
+    b_result = module.predict(src[0].unsqueeze(0), src_len[0].unsqueeze(0), trg_seq_len, 0, 1, 'beamsearch', 2)
+    print(g_result) # Nested List: List<List<Int>>
+    print(b_result) # Nested List: List<List<Int>>
